78bcb254e574aec4d4428de530d2191a36b152ad
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-18 16:06:21 -0400

    feat: adding all routes and controller methods

--- backend/src/controllers/budget.controller.ts
+++ backend/src/controllers/budget.controller.ts
@@ -4,6 +4,18 @@ export const getAllBudgets = (req: Request, res: Response) => {
   console.log("desde GET /api/v1/budgets");
 };
 
+export const getBudgetById = (req: Request, res: Response) => {
+  console.log("desde GET /api/v1/budgets/:id");
+};
+
 export const createBudget = (req: Request, res: Response) => {
   console.log("desde POST /api/v1/budgets/create");
+};
+
+export const updateBudgetById = (req: Request, res: Response) => {
+  console.log("desde PUT /api/v1/budgets/:id");
+};
+
+export const deleteBudgetById = (req: Request, res: Response) => {
+  console.log("desde DELETE /api/v1/budgets/:id");
 };
--- backend/src/routes/budget.routes.ts
+++ backend/src/routes/budget.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { createBudget, getAllBudgets } from "../controllers/budget.controller";
+import {
+  createBudget,
+  deleteBudgetById,
+  getAllBudgets,
+  getBudgetById,
+  updateBudgetById,
+} from "../controllers/budget.controller";
 
 const router = Router();
 
+// REST API
 router.get("/", getAllBudgets);
+router.get("/:id", getBudgetById);
 router.post("/", createBudget);
+router.put("/:id", updateBudgetById);
+router.delete("/:id", deleteBudgetById);
 
 export default router;
