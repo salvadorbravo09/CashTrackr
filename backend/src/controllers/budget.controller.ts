93667531b77e2f84f80591b961de2a01b9372215
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-18 20:34:25 -0400

    feat: create budget

--- backend/src/controllers/budget.controller.ts
+++ backend/src/controllers/budget.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import Budget from "../models/Budget";
 
 export const getAllBudgets = (req: Request, res: Response) => {
   console.log("desde GET /api/v1/budgets");
@@ -8,8 +9,14 @@ export const getBudgetById = (req: Request, res: Response) => {
   console.log("desde GET /api/v1/budgets/:id");
 };
 
-export const createBudget = (req: Request, res: Response) => {
-  console.log("desde POST /api/v1/budgets/create");
+export const createBudget = async (req: Request, res: Response) => {
+  try {
+    const budget = new Budget(req.body);
+    await budget.save();
+    res.status(201).json({ message: "Presupuesto creado correctamente" });
+  } catch (error) {
+    res.status(500).json({ error: "Hubo un error" });
+  }
 };
 
 export const updateBudgetById = (req: Request, res: Response) => {
