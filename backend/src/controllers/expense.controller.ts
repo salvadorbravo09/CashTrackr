90ac9cfe10401b384fe2b6561f33096e3bc8ee9c
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-24 18:30:20 -0400

    feat: update and delete expenses

--- backend/src/controllers/expense.controller.ts
+++ backend/src/controllers/expense.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Expense from "../models/Expense";
 
 export const getExpenseById = async (req: Request, res: Response) => {
-  
+  res.status(200).json(req.expense);
 };
 
 export const createExpense = async (req: Request, res: Response) => {
@@ -17,6 +17,22 @@ export const createExpense = async (req: Request, res: Response) => {
   }
 };
 
-export const updateExpenseById = async (req: Request, res: Response) => {};
+export const updateExpenseById = async (req: Request, res: Response) => {
+  try {
+    await req.expense.update(req.body);
+    res.status(200).json({ message: "Gasto actualizado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Hubo un error" });
+  }
+};
 
-export const deleteExpenseById = async (req: Request, res: Response) => {};
+export const deleteExpenseById = async (req: Request, res: Response) => {
+  try {
+    await req.expense.destroy();
+    res.status(200).json({ message: "Gasto eliminado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Hubo un error" });
+  }
+};
