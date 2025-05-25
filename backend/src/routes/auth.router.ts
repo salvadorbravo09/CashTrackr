0cb4fa75fda5e439d1f6380338a7a7a9bbbef39e
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-25 18:44:42 -0400

    feat: validation user register

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "../controllers/auth.controller";
+import { handleInputValidation } from "../middleware/validation";
 
 const router = Router();
 
-router.post("/create-account", createAccount);
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El nombre no puede ir vacio"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("El password debe tener al menos 8 caracteres"),
+  body("email").isEmail().withMessage("El email debe ser un email valido"),
+  handleInputValidation, // Si hay errores de validacion, se llama a este middleware
+  createAccount
+);
 
 export default router;
