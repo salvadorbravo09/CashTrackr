603b50173f5cb153f0b68e684b41c73f8cd54329
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-25 19:08:51 -0400

    feat: hash user password

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,33 @@
 import { Request, Response } from "express";
+import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
-export const createAccount = (req: Request, res: Response) => {
-  console.log("desde createAccount");
+export const createAccount = async (req: Request, res: Response) => {
+  try {
+    // Desestructuramos desde el cuerpo de la solicitud
+    const { name, email, password } = req.body;
+
+    // Prevenir usuarios duplicados
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists) {
+      res.status(400).json({ error: "El usuario ya existe con ese email" });
+      return;
+    }
+
+    // Encriptar la contraseña
+    const hashedPassword = await hashPassword(password);
+
+    // Crear un nuevo usuario con los datos proporcionados
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save(); // Guardar el usuario en la base de datos
+    res.status(201).json({ message: "Usuario creado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Hubo un error" });
+  }
 };
