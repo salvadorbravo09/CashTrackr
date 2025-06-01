f02f419e1f60d1746b179859918416d5f1186ce4
Salvador Bravo <sa.bravo0901@gmail.com>
2025-05-31 22:12:00 -0400

    feat: adding a mock to the expense controller

--- backend/src/tests/unit/budget.controller.test.ts
+++ backend/src/tests/unit/budget.controller.test.ts
@@ -1,6 +1,11 @@
 import { createRequest, createResponse } from "node-mocks-http";
 import { budgets } from "../mocks/budgets";
 import { getAllBudgets } from "../../controllers/budget.controller";
+import Budget from "../../models/Budget";
+
+jest.mock("../../models/Budget", () => ({
+  findAll: jest.fn(),
+}));
 
 describe("budget.controller.getAllBudgets", () => {
   it("should return 3 budgets", async () => {
@@ -13,7 +18,10 @@ describe("budget.controller.getAllBudgets", () => {
     });
     const res = createResponse();
 
+    (Budget.findAll as jest.Mock).mockResolvedValue(budgets);
     await getAllBudgets(req, res);
-    console.log(res._getJSONData)
+
+    const data = res._getJSONData();
+    console.log(data);
   });
 });
